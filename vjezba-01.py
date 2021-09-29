a7c08e6e4c0d03bfe092b783f46931595da71ac9
Mario Sever <34382956+mariosever@users.noreply.github.com>
2021-09-29 12:03:22 +0200

    Add files via upload

---
+++ vjezba-01.py
@@ -0,0 +1,24 @@
+# Varijable
+
+a = 5
+
+b = 3
+
+print(a)
+
+print(b)
+
+print(a + b)
+
+a = 10
+
+print(a + b)
+
+c = 3.14
+
+poruka1 = "Bravo hakeri!"
+
+poruka2 = "Danas učimo Python"
+
+print(poruka1 + " " + poruka2)
+
---
+++ vjezba-02.py
@@ -0,0 +1,14 @@
+# Input naredba
+
+ime = input("Unesi svoje ime: ")
+
+# print("Dobar dan, " + ime)
+
+# Izradi program koji će pitati korisnika za dva broja
+# broj a i broj b
+# ispiši zbroj na ekran
+
+a = int(input("Unesi broj a: "))
+b = int(input("Unesi broj b: "))
+
+print(a + b)
